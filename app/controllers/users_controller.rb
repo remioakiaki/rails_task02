9af96d71613577864b36e9ba366ebbbeba0eddcf
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2020-07-04 11:53:54 +0900

    resourcesを使ったCRUDアプリの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,9 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def show
+    @user = User.find(params[:id])
+  end
   def create
     User.create(user_params)
   end
